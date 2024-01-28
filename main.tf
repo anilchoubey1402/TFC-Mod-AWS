65dddc09610054d8389ac64f0efa46f25ead24e7
Anil choubey <anil_choubey@hotmail.com>
2024-01-29 02:16:31 +0530

    correct main1

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ region = "us-east-1"
 }
 
       resource "aws_instance" "web" {
-       ami = ami-0c7217cdde317cfec
+       ami = "ami-0c7217cdde317cfec"
        instance_type = "t2.micro"
 
   tags = {
