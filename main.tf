c3750a08599a9acadfc3bf7c80ebb0c0a9152cf3
Anil choubey <anil_choubey@hotmail.com>
2024-01-29 02:41:02 +0530

    destroy EC2 new

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-/*terraform {
+terraform {
   required_providers {
     aws = {
         source = "hashicorp/aws"
@@ -12,7 +12,7 @@ provider "aws" {
 region = "us-east-1"
 }
 
-      resource "aws_instance" "web" {
+      /*resource "aws_instance" "web" {
        ami = "ami-0c7217cdde317cfec"
        instance_type = "t2.micro"
 
